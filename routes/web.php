91bae18b1cabc60645794ba638cdec8c8762b929
Ruhul Amin <43258231+ruhulamin1398@users.noreply.github.com>
2022-05-29 13:48:18 +0600

    index

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/', function () {
 
     $interiorProjects = Project::where('category_id', '1')->with('images')->orderBy('serial', 'asc')->get()->take(6);
     $constructionProjects = Project::where('category_id', '2')->with('images')->orderBy('serial', 'asc')->get()->take(6);
-    $allProjectsList = Project::with('images')->orderBy('serial', 'asc')->get();
+    $allProjectsList = Project::with('images')->orderBy('serial', 'asc')->get()->take(6);
     
     return view('frontend.index', compact('interiorProjects', 'constructionProjects','allProjectsList'));
 })->name('home-index');
