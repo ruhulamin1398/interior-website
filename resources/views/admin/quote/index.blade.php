d25f795d86df019f742089da9997ee1e70f93606
jobaed <jobaedbhuiyan34@gmail.com>
2022-07-25 20:40:00 +0600

    work in quote admin panel

--- resources/views/admin/quote/index.blade.php
+++ resources/views/admin/quote/index.blade.php
@@ -10,9 +10,29 @@
     @endif
     <h2 class="py-3">Quote Data</h2>
     @foreach ($quotes as $quote)
-    <div class="container py-2 my-3">
+    <div class="container px-4 py-3 my-3 shadow rounded">
         <h3><b> Name: </b><span class="fw-normal">{{ $quote->name }}</span></h3>
-        <h5></h5> Contact:
+        <h5 class="py-2">Contact: <span class="fw-normal" style="font-size: 16px">{{ $quote->phone }} | </span> <span
+                class="fw-normal" style="font-size: 18px">{{ $quote->email }}</span></h5>
+        <h5>Project Type: <span class="fw-normal" style="font-size: 18px">{{ $quote->projecttype }} </span> </h5>
+        <hr>
+        <div class="py-3">
+            <h6>What Is Your Estimated Budget For This Project?</h6>
+            <span style="font-size: 16px">Answer: {{ $quote->budget }}</span>
+        </div>
+        <div class="py-3">
+            <h6>Is There A Particular Deadline For This Project?</h6>
+            <span style="font-size: 16px">Answer: {{ $quote->contact }}</span>
+        </div>
+        <div class="py-3">
+            <h6>Have You Worked With A Professional Interior Design Firm Before?</h6>
+            <span style="font-size: 16px">Answer: {{ $quote->experience }}</span>
+        </div>
+        <div class="py-3">
+            <h6>Message</h6>
+            <span style="font-size: 16px">" {{ $quote->message }} "</span>
+        </div>
+
     </div>
     @endforeach
 
