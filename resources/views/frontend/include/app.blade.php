6611e2c1138f282c7327a41181480b5af46e3262
Ruhul Amin <43258231+ruhulamin1398@users.noreply.github.com>
2022-07-27 12:38:50 +0600

    add page embaded

--- resources/views/frontend/include/app.blade.php
+++ resources/views/frontend/include/app.blade.php
@@ -93,7 +93,8 @@
 
 
 
-
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fkarigorinterior.bd&tabs=timeline&width=340&height=350&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                
 
 
 
